cad92de77fc9486fef2a67a82e77168b7a75f08c
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-06-14 19:24:14 +0500

    7% work completed

--- app.js
+++ app.js
@@ -12,10 +12,18 @@ axios(
   )
     .then((res) => {
       console.log(res.data);
+      card.innerHTML = `<div class="card d-flex justify-content-center" style="width: 22rem;">
+      <div class="card-body">
+        <h2 class="card-title">${res.data.location.name}</h2>
+        <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+      `
     })
     .catch((err) => {
       console.log(err);
     });
-card.innerHTML = 
-
 })
